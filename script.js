14121f6edadb1d181e0d74b200bafd73df285749
casgeosn-cmd <cas.geosn@gmail.com>
2026-08-29 22:27:29 -0400

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ sounds.forEach((sound) => {
     btn.innerText = sound;
     DocumentFragment.getElementaryById('buttons').appendChild(btn);
 });
+
+btn.addEventListener('click', () => {
+    document.getElementById(sound).onplay();
+});
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime=0;
+
+    })
+};
