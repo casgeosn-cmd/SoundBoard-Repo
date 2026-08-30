6c0f15bd060c2f12e69e47c259e2d58d8f76bee0
casgeosn-cmd <cas.geosn@gmail.com>
2026-08-29 21:58:02 -0400

    modified:   index.html 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    DocumentFragment.getElementaryById('buttons').appendChild(btn);
+});
