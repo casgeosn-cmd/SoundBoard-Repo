711bd9de9baca9c61a084c2103aefc27ccfc503d
casgeosn-cmd <cas.geosn@gmail.com>
2026-08-30 20:42:22 -0400

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 sounds.forEach((sound) => {
      // create the button 
-     const btn = document.createElement('button');
+    const btn = document.createElement('button');
      // add a class btn to the element
      btn.classList.add('btn');
      // button label text = string value in the sounds array
      btn.innerText = sound;
+     
      // add button to the DOM
      document.getElementById('buttons').appendChild(btn);
 });
